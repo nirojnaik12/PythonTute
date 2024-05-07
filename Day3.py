183a81c75c484550132960483bf6d6990cf6443a
nirojnaik12 <nirojnaik12@gmail.com>
2024-05-07 09:55:38 +0530

    slicing of string

--- Day3.py
+++ Day3.py
@@ -1,7 +1,7 @@
 # in previous class
 myName = "Niroj Kumar Naik\""
 
-print(myName)
+print(myName)  # Niroj Kumar Naik"
 
 print(myName[5])
 
@@ -24,3 +24,23 @@ if "Ni" in myName:
 if "Ni " not in myName:
     print("No, 'Ni ' is not in myName variable")
 
+# ------------character slicing----------------
+
+# the variable is: myName = "Niroj Kumar Naik\""
+
+#                       -OutPut-        begin          ends
+#                                     (include)     (exclude)
+print(myName[2:8])      # roj Ku          2             8
+print(myName[2:8:2])    # rjK             2             8
+print(myName[2::2])     # rjKmrNi"        2             16
+print(myName[::2])      # NrjKmrNi"       0             16
+print(myName[-5:-1])    # Naik           -5          -1 or 16
+
+# another way to use slicing
+sliceOne = slice(3)
+sliceTwo = slice(3, -1, 2)
+
+#                            -OutPut-        begin          ends
+#                                          (include)     (exclude)
+print(myName[sliceOne])      # Nir            0              3
+print(myName[sliceTwo])      # o ua ak        3           -1 or 16
